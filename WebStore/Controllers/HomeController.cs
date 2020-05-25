4cd1f279ecf829b4e4232d28583cb0038bd60666
evgeny.ponamarev <evgeny.ponamarev@megafon.ru>
2020-05-25 11:46:05 +0300

    Первое представление.

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace WebStore.Controllers
     {
         public IActionResult Index()
         {
-            return Content("First controller");
+            return View();
         }
         public IActionResult AnotherAction()
         {
