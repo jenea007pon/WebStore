e74976e6a663473e7d945dffbef323bcdaa18f2f
evgeny.ponamarev <evgeny.ponamarev@megafon.ru>
2020-06-08 14:26:56 +0300

    Определении секции и бренда

---
+++ WebStore.Domain/Entityes/Brand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+using WebStore.Domain.Entityes.Base;
+using WebStore.Domain.Entityes.Base.interfaces;
+
+namespace WebStore.Domain.Entityes
+{
+    public class Brand : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+    }
+}
---
+++ WebStore.Domain/Entityes/Section.cs
@@ -0,0 +1,11 @@
+﻿using WebStore.Domain.Entityes.Base;
+using WebStore.Domain.Entityes.Base.interfaces;
+
+namespace WebStore.Domain.Entityes
+{
+    public class Section : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+        public int? ParentId { get; set; }
+    }
+}
