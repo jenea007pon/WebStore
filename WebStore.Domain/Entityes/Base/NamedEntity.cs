9951fbdb19c5384f1884356e73efd7380243f50f
evgeny.ponamarev <evgeny.ponamarev@megafon.ru>
2020-06-08 13:04:42 +0300

    Базовые классы сущностей.

---
+++ WebStore.Domain/Entityes/Base/NamedEntity.cs
@@ -0,0 +1,10 @@
+﻿using WebStore.Domain.Entityes.Base.interfaces;
+
+namespace WebStore.Domain.Entityes.Base
+{
+    public abstract class NamedEntity : BaseEntity, INamedEntity
+    {
+        public string Name { get; set; }
+    }
+
+}
