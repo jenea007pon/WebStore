834d401d848c3178819e5b7b788e32376db57e02
evgeny.ponamarev <evgeny.ponamarev@megafon.ru>
2020-06-08 17:55:01 +0300

    Регистрация сервиса каталога товаров. Модели представления секции и бренда.

---
+++ WebStore/ViewModels/SectionViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities.Base.interfaces;
+
+namespace WebStore.ViewModels
+{
+    public class SectionViewModel : INamedEntity, IOrderedEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+        public List<SectionViewModel> ChildSections { get; set; } = new List<SectionViewModel>();
+
+    }
+    public class BrandViewModel:INamedEntity, IOrderedEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Order { get; set; }
+    }
+}
