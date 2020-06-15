cdae9e796a4397fec0394d6e037f542189ff8f36
evgeny.ponamarev <evgeny.ponamarev@megafon.ru>
2020-06-15 12:39:50 +0300

    Определние контекста БД

--- WebStore.DAL/Context/WebStoreDB.cs
+++ WebStore.DAL/Context/WebStoreDB.cs
@@ -1,8 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebStore.Domain.Entities;
 
 namespace WebStore.DAL.Context
 {
-    class WebStoreDB:DbContext
+    class WebStoreDB : DbContext
     {
+        public DbSet<Product> Products { get; set; }
+        publi DbSet<Section> Sections { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public WebStoreDB(DbContextOptions<WebStoreDB> Options) : base(Options) { }
+        {
+
+        }
+        
     }
 }
